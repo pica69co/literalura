5cb77ab468aaf95e2ca55ecb7d7af2fd9e028c9f
pica69co <oscarwarrieta@gmail.com>
2024-05-20 16:39:00 -0500

    Initial commit

---
+++ src/main/java/com/warrieta/literalura/literalura/principal/Principal.java
@@ -0,0 +1,7 @@
+package com.warrieta.literalura.literalura.principal;
+
+public class Principal {
+    public void menuPrincipal() {
+        System.out.println("*** Welcome! Menu Principal ***");
+    }
+}
